ab50e6f1f2218fdb37c64038027c0cd83dff633b
Natalia Bandzeladze <na.bandzeladze@gmail.com>
2021-02-04 20:10:14 +0300

    Completed task 5
    Add another task.

--- JS_task_5/js/script.js
+++ JS_task_5/js/script.js
@@ -1,3 +1,4 @@
+//The name of JavaScript
 officialName = prompt("What is the “official” name of JavaScript?", "");
 
 if (officialName == "ECMAScript"){
@@ -6,3 +7,25 @@ if (officialName == "ECMAScript"){
 else {
     alert("You don’t know? ECMAScript!");
 }
+
+//Show the sign
+userNumber = prompt("Enter the number", "");
+
+if (userNumber > 0) {
+    alert(1);
+}
+else if (userNumber < 0) {
+    alert(-1);
+}
+else {
+    alert(0);
+}
+
+//Rewrite 'if' into '?'
+let result = (a + b < 4) ? 'Below' : 'Over';
+
+//Rewrite 'if..else' into '?'
+let message = (login == 'Employee') ? 'Hello' :
+  (login == 'Director') ? 'Greetings' :
+  (login == '') ? 'No login' :
+  '';
