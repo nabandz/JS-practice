851a96ca1d74010e52ba4eafe4cc48a8237cc523
Natalia Bandzeladze <na.bandzeladze@gmail.com>
2021-02-04 19:54:35 +0300

    Completed task 5
    Using the if..else construct, write the code which asks: ‘What is the official name of JavaScript?’

---
+++ JS_task_5/js/script.js
@@ -0,0 +1,8 @@
+officialName = prompt("What is the “official” name of JavaScript?", "");
+
+if (officialName == "ECMAScript"){
+    alert("Right!");
+} 
+else {
+    alert("You don’t know? ECMAScript!");
+}
