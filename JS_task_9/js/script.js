386ae15cd817285aebd4b78ea2b548c9af1b99aa
Natalia Bandzeladze <na.bandzeladze@gmail.com>
2021-02-07 20:27:55 +0300

    Completed task 9

---
+++ JS_task_9/js/script.js
@@ -0,0 +1,13 @@
+//Replace "for" with "while"
+//"for"
+for (let i = 0; i < 3; i++) {
+    alert( `number ${i}!` );
+}
+
+//"while"
+let i = 0;
+
+while(i < 3) {
+    alert( `number ${i}!` );
+    i++;
+}
