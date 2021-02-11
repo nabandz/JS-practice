5c91f12e7b9de50fc194930655073e70870a09bb
Natalia Bandzeladze <na.bandzeladze@gmail.com>
2021-02-11 19:02:12 +0300

    Completed task 20
    Added correct script.js file

--- JS_task_20/js/script.js
+++ JS_task_20/js/script.js
@@ -1,39 +1,16 @@
-//Bad style
+//Check for emptiness
 
-/* function pow(x,n)
-{
-  let result=1;
-  for(let i=0;i<n;i++) {result*=x;}
-  return result;
+function isEmpty(obj) {
+  for (let key in obj) {
+      return false;
+    }
+    return true;
 }
 
-let x=prompt("x?",''), n=prompt("n?",'')
-if (n<=0)
-{
-  alert(`Power ${n} is not supported, please enter an integer number greater than zero`);
-}
-else
-{
-  alert(pow(x,n))
-} */
+let schedule = {};
 
-function pow(x, n) {
-  let result = 1;
+alert( isEmpty(schedule) ); // true
 
-  for (let i = 0; i < n; i++) {
-    result *= x;
-  }
+schedule["8:30"] = "get up";
 
-  return result;
-}
-
-let x = prompt("x?", ''); 
-let n = prompt("n?", '');
-
-if (n <= 0) {
-  alert(`Power ${n} is not supported,
-    please enter an integer number greater than zero`);
-}
-else {
-  alert( pow(x, n) );
-}
+alert( isEmpty(schedule) ); // false
