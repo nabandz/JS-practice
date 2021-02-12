661b046b42a226a72a5893fa003b71642d2dbaae
Natalia Bandzeladze <na.bandzeladze@gmail.com>
2021-02-12 20:15:09 +0300

    Completed task 24

---
+++ JS_task_24/js/script.js
@@ -0,0 +1,20 @@
+//Chaining
+
+let ladder = {
+  step: 0,
+  up() {
+    this.step++;
+    return this;
+  },
+  down() {
+    this.step--;
+    return this;
+  },
+  showStep: function() {
+    alert( this.step );
+    return this;
+  }
+};
+
+ladder.up().up().down().showStep(); // 1
+
