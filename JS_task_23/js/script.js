431b74057f2fb8298d228807e6e6d69aceb591d5
Natalia Bandzeladze <na.bandzeladze@gmail.com>
2021-02-12 20:00:53 +0300

    Completed task 23

---
+++ JS_task_23/js/script.js
@@ -0,0 +1,20 @@
+//Create a calculator
+
+let calculator = {
+  read() {
+    this.val1 = +prompt("Enter value 1","");
+    this.val2 = +prompt("Enter value 2","");;
+  },
+
+  sum() {
+    return this.val1 + this.val2;
+  },
+
+  mul() {
+    return this.val1 * this.val2;
+  },
+};
+
+calculator.read();
+alert( calculator.sum() );
+alert( calculator.mul() );
