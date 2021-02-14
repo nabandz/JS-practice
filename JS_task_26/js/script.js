8377601ae1f9b0de7ece2044ecdab21ecb6d2448
Natalia Bandzeladze <na.bandzeladze@gmail.com>
2021-02-14 18:03:15 +0300

    Completed task 26

---
+++ JS_task_26/js/script.js
@@ -0,0 +1,17 @@
+//Create new Accumulator
+
+function Accumulator(startingValue) {
+  this.value = startingValue;
+
+  this.read = function() {
+    this.newValue = +prompt("Enter new value","");
+    this.value +=  this.newValue;
+  }
+}
+
+let accumulator = new Accumulator(1); // initial value 1
+
+accumulator.read(); // adds the user-entered value
+accumulator.read(); // adds the user-entered value
+
+alert(accumulator.value); // shows the sum of these values
