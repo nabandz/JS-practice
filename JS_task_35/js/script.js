f6e194010a18f59cd1126dc9a759e1e0c3e2ccb1
Natalia Bandzeladze <na.bandzeladze@gmail.com>
2021-02-22 16:51:31 +0300

    Completed task 35

---
+++ JS_task_35/js/script.js
@@ -0,0 +1,7 @@
+//Array operations.
+
+let styles = ["Jazz", "Blues", "Blues"];
+styles.push("Rock-n-Roll");
+styles[Math.floor((styles.length - 1) / 2)] = "Classics";
+alert ( styles.shift() );
+styles.unshift("Rap", "Reggae");
