92df72e4653782625cd61c49594eb47fe601be71
Natalia Bandzeladze <na.bandzeladze@gmail.com>
2021-02-24 20:51:02 +0300

    Completed task 42

---
+++ JS_task_42/js/script.js
@@ -0,0 +1,12 @@
+//Copy and sort array
+
+function copySorted(arr) {
+    return arr.slice().sort();
+}
+
+let arr = ["HTML", "JavaScript", "CSS"];
+
+let sorted = copySorted(arr);
+
+alert( sorted ); // CSS, HTML, JavaScript
+alert( arr ); // HTML, JavaScript, CSS (no changes)
