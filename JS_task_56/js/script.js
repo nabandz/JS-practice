cbe4c6c04c7c44e0892588625aed89d3b9587ae2
Natalia Bandzeladze <na.bandzeladze@gmail.com>
2021-03-03 01:30:18 +0300

    Completed task 56

---
+++ JS_task_56/js/script.js
@@ -0,0 +1,15 @@
+//European weekday
+
+function getLocalDay(date) {
+
+    let day = date.getDay();
+
+    if (day == 0) {
+        day = 7;
+    }
+
+    return day;
+}
+
+let date = new Date(2012, 0, 8);
+alert( getLocalDay(date) );
