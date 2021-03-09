7b6f4a0c888ad6ef366c63e9bd932d66f1856ab3
Natalia Bandzeladze <na.bandzeladze@gmail.com>
2021-03-10 01:04:44 +0300

    Completed task 72

---
+++ JS_task_72/js/script.js
@@ -0,0 +1,17 @@
+//Sort by field
+
+function byField(field) {
+    return (a, b) => a[field] > b[field] ? 1 : -1;
+}
+
+let users = [
+    { name: "John", age: 20, surname: "Johnson" },
+    { name: "Pete", age: 18, surname: "Peterson" },
+    { name: "Ann", age: 19, surname: "Hathaway" }
+];
+
+users.sort(byField('name'));
+users.forEach(user => alert(user.name)); // Ann, John, Pete
+
+users.sort(byField('age'));
+users.forEach(user => alert(user.name)); // Pete, Ann, John
